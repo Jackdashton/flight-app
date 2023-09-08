0a4bb8d7798619ded92c1ba02f769c2f52021773
jack ashton <jackdashton@outlook.com>
2023-09-08 16:07:37 +0100

    can map through array of flights & return all id's with unique key

--- client/src/components/Results.jsx
+++ client/src/components/Results.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
 
-function Results() {
+function Results({ data }) {
+  const flightsArray = data.flight;
+  if (flightsArray) {
+    return (
+      <ul>
+        {flightsArray.map((flight) => (
+          <li key={flight.$.id}>{flight.$.id}</li>
+        ))}
+      </ul>
+    );
+  }
   return (
-    <>
-      <h2>Results</h2>
-    </>
+    <p> Can't find the Data</p>
   )
 }
+
+Results.propTypes = {
+  data: PropTypes.object,
+};
 
 export default Results;
