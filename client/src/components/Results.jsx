f0b2d32b5fd8ca66b1a32282a938696f5e10e6fe
jack ashton <jackdashton@outlook.com>
2023-09-08 17:50:55 +0100

    cards created to display flight information

--- client/src/components/Results.jsx
+++ client/src/components/Results.jsx
@@ -8,10 +8,44 @@ function Results({ data }) {
       <ul>
         {flightsArray.map((flight) => (
           <li key={flight.$.id}>
-            <p>ID: {flight.$.id}</p>
             <p>Carrier: {flight.$.carrier}</p>
             <p>Departure Airport: {flight.$.depair}</p>
             <p>Destination Airport: {flight.$.destair}</p>
+            <p>
+              Inbound Departure Time/Date: {flight.$.indepartdate}
+              <br />
+              {flight.$.indeparttime}
+            </p>
+            <p>
+              Inbound Arrival Time/Date: {flight.$.inarrivaldate}
+              <br />
+              {flight.$.inarrivaltime}
+            </p>
+            <p>Inbound Arrival Code: {flight.$.inarrivecode}</p>
+            <p>Inbound Booking Class: {flight.$.inbookingclass}</p>
+            <p>Inbound Carrier Code: {flight.$.incarriercode}</p>
+            <p>Inbound Departure Code: {flight.$.indepartcode}</p>
+            <p>Inbound Flight Class: {flight.$.inflightclass}</p>
+            <p>Inbound Flight Number: {flight.$.inflightno}</p>
+            <p>
+              Price: {flight.$.originalprice} {flight.$.originalcurrency}
+            </p>
+            <p>
+              Outbound Departure Time/Date: {flight.$.outdepartdate}
+              <br />
+              {flight.$.outdeparttime}
+            </p>
+            <p>
+              Outbound Arrival Time/Date: {flight.$.outarrivaldate}
+              <br />
+              {flight.$.outarrivaltime}
+            </p>
+            <p>Outbound Flight Class: {flight.$.outflightclass}</p>
+            <p>Outbound Flight Number: {flight.$.outflightno}</p>
+            <p>Outbound Booking Class: {flight.$.outbookingclass}</p>
+            <p>Outbound Carrier Code: {flight.$.outcarriercode}</p>
+            <p>Reservation: {flight.$.reservation}</p>
+            <p>One way: {flight.$.oneway === "1" ? "Yes" : "No"}</p>
           </li>
         ))}
       </ul>
