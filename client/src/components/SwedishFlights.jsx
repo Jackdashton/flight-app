71c68663c80b8843661e6eeeb213fb461b1e5771
jack ashton <jackdashton@outlook.com>
2023-09-13 20:17:10 +0100

    styling for swedish flights complete

--- client/src/components/SwedishFlights.jsx
+++ client/src/components/SwedishFlights.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { swedishIATACodes } from "../swedishAirports.data.js";
 import PropTypes from "prop-types";
+import styles from "./SwedishFlights.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlane } from "@fortawesome/free-solid-svg-icons";
 
 function SwedishFlights({ data }) {
   const swedishCodes = swedishIATACodes;
   const flightsArray = data.flight;
   const [count, setCount] = React.useState(0);
-  const [totalFlights, setTotalFlights] = React.useState(0)
+  const [totalFlights, setTotalFlights] = React.useState(0);
   const [swedishFlights, setSwedishFlights] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
 
@@ -17,17 +20,16 @@ function SwedishFlights({ data }) {
       let totalFlights = 0;
 
       flightsArray.forEach((flight) => {
-
         const hasSegments = flight.segments && flight.segments[0].segment;
 
-        if (!hasSegments){
+        if (!hasSegments) {
           totalFlights++;
         } else if (hasSegments) {
           const segments = flight.segments[0].segment;
 
           segments.map((segment) => {
             totalFlights++;
-          })
+          });
         }
 
         if (swedishCodes.includes(flight.$.destair)) {
@@ -54,24 +56,50 @@ function SwedishFlights({ data }) {
     }
   }, [flightsArray]);
 
-
-  const percentageOfFlights = Math.round(count / totalFlights * 10000)/100;
+  const percentageOfFlights = Math.round((count / totalFlights) * 10000) / 100;
 
   return (
-    <>
-      <p>Total Flights (including segment flights): <strong>{totalFlights}</strong></p>
-      <p>What percentage of total flights fly into Sweden: <strong>{percentageOfFlights}%</strong></p>
-      <p>Number of flights flying into Sweden: <strong>{count}</strong></p>
+    <div className={styles.container}>
+      <div className={styles.answers}>
+        <p>
+          Total Flights (including segment flights):{" "}
+          <strong>{totalFlights}</strong>
+        </p>
+        <p>
+          What percentage of total flights fly into Sweden:{" "}
+          <strong>{percentageOfFlights}%</strong>
+        </p>
+        <p>
+          Number of flights flying into Sweden: <strong>{count}</strong>
+        </p>
+      </div>
+
       {isLoading ? (
         <p>Flight Data Loading....</p>
       ) : (
         <ul>
           {swedishFlights.map((flight, index) => (
-            <li key={index}>{flight.$.destair}</li>
+            <li key={index} className={styles.card}>
+              <div className={styles.column}>
+                <div className={styles.destination}>
+                  <div className={styles.out}>
+                    <div className={styles.outDep}>
+                      <p>{flight.$.depair}</p>
+                    </div>
+                    <p>
+                      -- <FontAwesomeIcon icon={faPlane} /> --
+                    </p>
+                    <div className={styles.outArr}>
+                      <p>{flight.$.destair}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
           ))}
         </ul>
       )}
-    </>
+    </div>
   );
 }
 
